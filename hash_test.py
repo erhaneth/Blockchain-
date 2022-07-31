5a871753a174b2e7c0a45d4ed257b68b9593a389
“erhaneth” <gumusucb21@gmail.com>
2022-07-31 12:31:12 -0700

    Some review

--- hash_test.py
+++ hash_test.py
@@ -1,13 +1,13 @@
 # python's
 import hashlib
-from hmac import digest
+# from hmac import digest
 
 # encode a string to a hash -- encode to utf-8
 encode = 'A'.encode() # no args encode to utf-8
 
 
-# digest into a has
-print(hashlib.sha256(encode).hexdigest())
+# decode a hash to a string -- decode from utf-8
+digest = hashlib.sha256(encode).hexdigest()
 
 print(digest)
 print(digest == hashlib.sha256('A'.encode()).hexdigest())
